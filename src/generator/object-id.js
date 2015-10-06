871894888df0fb4924087b9b297f2097b100ba02
PhucPNT <phuc@phucpnt.com>
2015-10-06 15:25:22 +0700

    latest update

--- src/generator/object-id.js
+++ src/generator/object-id.js
@@ -2,11 +2,13 @@
  * Created by Phuc on 10/1/2015.
  */
 
+var _ = require('lodash');
+
 module.exports = function () {
 
     return {
       get $() {
-        return 'test Object ID...';
+        return _.uniqueId();
       }
     };
 
