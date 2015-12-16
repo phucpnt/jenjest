f9de47324aef1860a58c5ac34af8aa40d9947b20
PhucPNT <phuc@phucpnt.com>
2015-12-16 13:09:40 +0700

    add more test case

--- test/spec/compile/value-generate-spec.js
+++ test/spec/compile/value-generate-spec.js
@@ -4,13 +4,13 @@
 
 
 import makeCompile from '../../../src/compile'
-import makeValueGenerateCompile from '../../../src/compiler/value-generate'
+import makeValueGenerateCompile from '../../../src/compile/value-generate'
 import ObjectId from '../../../src/generator/object-id'
 
 
 fdescribe('Compile with value generated', () => {
 
-  it('should work', () => {
+  it('should work with string template', () => {
     var finalCompile = makeValueGenerateCompile({'objectId': ObjectId})(makeCompile());
 
     var data = finalCompile(require('../../resources/json-generator.jsample.txt'));
@@ -18,7 +18,18 @@ fdescribe('Compile with value generated', () => {
     expect(typeof data).toEqual('object');
     expect(JSON.stringify(data)).not.toContain('objectId');
 
+  });
+
+  it('should work with direct field generate', () => {
+    var finalCompile = makeValueGenerateCompile({'objectId': ObjectId})(makeCompile());
+
+    var data = finalCompile(require('../../resources/json-generator-direct-field.jsample.txt'));
+
+    expect(typeof data).toEqual('object');
+    expect(JSON.stringify(data)).not.toContain('objectId');
+    console.log(data);
 
   });
+
 
 });
