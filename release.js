e0bdcca7a701664e9473c53982e8e088c012c187
PhucPNT <phuc@phucpnt.com>
2016-01-02 23:55:13 +0700

    build release

--- release.js
+++ release.js
@@ -1,12 +1,16 @@
-
-var package = require('./package.json');
+var fs = require('fs');
 
 !function increaseMinorVersion(){
+  var package = require('./package.json');
   var currentVersion = package.version.split('.');
   var minorNum = currentVersion[2];
   currentVersion[2] = parseInt(minorNum)+1;
 
-  var nextVersion = currentVersion.join('.')
+  var nextVersion = currentVersion.join('.');
+  package.version = nextVersion;
+
+  fs.writeFileSync('package.json', JSON.stringify(package, null, 2), {encoding: 'utf8'});
+
 }();
 
 require('./build');
